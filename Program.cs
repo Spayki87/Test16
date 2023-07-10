2eedda35f543a7ff02fb9c96450a63e97cf6423d
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-10 09:34:23 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,20 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Пример_с_N
+namespace Кратные_числа
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int N = 12;
-            int maxNamber = 999;
-            int remainder = maxNamber % N;
-            int difference = N - remainder;
+            Random random = new Random();
 
-            int count = difference / N + 1;
+            int minNumber = 1;
+            int maxNumber = 27;
+            int number = random.Next(minNumber, maxNumber + 1);
+            int minRangeNumber = 100;
+            int maxRangeNumber = 999;
+            int multiplesCounter = 0;
 
-            Console.WriteLine(count);
+            Console.WriteLine(number + " - рандомное число");
+
+            for (int i = number; i <= maxRangeNumber; i += number)
+            {
+                if (i >= minRangeNumber)
+                {   
+                    multiplesCounter++;
+                    Console.WriteLine(multiplesCounter + " " + i);
+                }
+            }
+
+            Console.WriteLine(multiplesCounter + " - количество кратных чисел.");
         }
     }
 }
