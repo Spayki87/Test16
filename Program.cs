f8614d7dbc7848cd1f893d9a34e5cac4fe89742a
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-10 09:32:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пример_с_N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 12;
+            int maxNamber = 999;
+            int remainder = maxNamber % N;
+            int difference = N - remainder;
+
+            int count = difference / N + 1;
+
+            Console.WriteLine(count);
+        }
+    }
+}
